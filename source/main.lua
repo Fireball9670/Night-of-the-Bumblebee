3245f123e1b554fc9149a8da089aa561a1fa50d0
Henri Wagener <fireball9670@gmail.com>
2024-10-11 13:29:17 +0200

    initial commit

---
+++ source/main.lua
@@ -0,0 +1,32 @@
+import "CoreLibs/object"
+import "CoreLibs/sprites"
+import "CoreLibs/graphics"
+
+import "sprites"
+import "menu"
+
+-- make pointers
+local gfx <const> = playdate.graphics
+local sprite <const> = gfx.sprite
+
+local player = Player()
+
+-- init() runs once at game start
+local function init()
+    player:add()
+
+    showMenu()
+end
+
+-- update() runs every frame
+function playdate.update()
+    -- update all sprites
+    sprite.update()
+
+    if playdate.buttonJustPressed(playdate.kButtonA) then
+        hideMenu()
+    end
+
+end
+
+init()
---
+++ source/sprites/player.lua
@@ -0,0 +1,43 @@
+local gfx <const> = playdate.graphics
+
+class("Player").extends(gfx.sprite)
+
+-- make new sprite
+local imageSpritePlayer = gfx.image.new("assets/SpaceShip")
+
+local velocity = 0
+
+function Player:init()
+    -- create the player
+    Player.super.init(self, imageSpritePlayer)
+
+    self:moveTo(200, 60)
+
+    velocity = 0
+end
+
+function Player:update()
+    -- update the player
+    Player.super.update(self)
+
+    -- rotate sprite based on the rotation of the Crank[tm]
+    local crankPosition = playdate.getCrankPosition()
+    self:setRotation(crankPosition)
+
+    -- add velocity when A is pressed
+    if playdate.buttonIsPressed(playdate.kButtonA) then
+        velocity = 5
+    else
+        velocity = 0
+    end
+
+    -- lua needs angles in radians
+    local crankPositionRadians = math.rad(crankPosition)
+    -- calculate the x and y velocity
+    local vX,vY = velocity * math.cos(crankPositionRadians), velocity * math.sin(crankPositionRadians)
+
+    self:moveBy(vX, vY)
+
+    -- wrap around the screen
+    self:moveTo(self.x % 400, self.y % 240)
+end
